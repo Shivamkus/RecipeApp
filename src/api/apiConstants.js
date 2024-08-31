0b340889b0b27f969f500ac9363e5c223462768a
Shivamkus <kushwahshivam065@.com>
2024-08-31 09:19:33 +0530

    first commit

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,2 +1,2 @@
-export const APP_ID = "YOUR EDAMAM APP ID";
-export const APP_KEY = "YOUR EDAMAM APP KEY";
+export const APP_ID = "b752f004";
+export const APP_KEY = "60cfe2c93f76b9392dad9b1d7977c386%09";
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./redux/store/store.js";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </Provider>
-);
+  </StrictMode>,
+)
